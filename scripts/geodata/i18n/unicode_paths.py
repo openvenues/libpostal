9c935c9cc70ba992763a37588e3565ab1602ce58
Al <albarrentine@gmail.com>
2015-06-01 17:13:06 -0400

    [fix] Base data dir path

--- scripts/geodata/i18n/unicode_paths.py
+++ scripts/geodata/i18n/unicode_paths.py
@@ -4,7 +4,8 @@ import sys
 this_dir = os.path.realpath(os.path.dirname(__file__))
 sys.path.append(os.path.realpath(os.path.join(os.pardir, os.pardir)))
 
-UNICODE_DATA_DIR = os.path.join(this_dir, os.pardir, os.pardir, os.pardir,
-                                'data', 'unicode')
+DATA_DIR = os.path.join(this_dir, os.pardir, os.pardir, os.pardir, 'data')
+
+UNICODE_DATA_DIR = os.path.join(DATA_DIR, 'unicode')
 
 CLDR_DIR = os.path.join(UNICODE_DATA_DIR, 'cldr')
