816a0408ab1e644fd493652329ff27ed5eebb3e7
Al <albarrentine@gmail.com>
2015-06-02 12:30:56 -0400

    [numex] numex_rule.h

---
+++ src/numex_rule.h
@@ -0,0 +1,22 @@
+#ifndef NUMEX_RULE_H
+#define NUMEX_RULE_H
+
+#include <stdlib.h>
+#include "numex.h"
+
+typedef struct numex_rule_source {
+    char *key;
+    numex_rule_t rule;
+} numex_rule_source_t;
+
+typedef struct numex_language_source {
+    char *name;
+    size_t rule_index;
+    size_t num_rules;
+    size_t ordinal_indicator_index;
+    size_t num_ordinal_indicators;
+    size_t stopword_index;
+    size_t num_stopwords;
+} numex_language_source_t;
+
+#endif
