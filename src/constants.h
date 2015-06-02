958c219b882d26b62a58b212cfb9678f6158e075
Al <albarrentine@gmail.com>
2015-06-02 12:25:58 -0400

    [utils] constants.h

---
+++ src/constants.h
@@ -0,0 +1,10 @@
+#ifndef LIBPOSTAL_CONSTANTS_H
+#define LIBPOSTAL_CONSTANTS_H
+
+#include <stdlib.h>
+#include <string.h>
+
+#define NAMESPACE_SEPARATOR_CHAR "|"
+#define NAMESPACE_SEPARATOR_CHAR_LEN strlen(NAMESPACE_SEPARATOR_CHAR)
+
+#endif
