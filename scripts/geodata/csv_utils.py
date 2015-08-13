b54ff95ecc4e527e961a223f557ea240fbed1de1
Al <albarrentine@gmail.com>
2015-08-13 18:18:56 -0400

    [mv] csv_utils

--- scripts/geodata/csv_utils.py
+++ scripts/geodata/csv_utils.py
@@ -1,5 +1,7 @@
 import csv
 
+csv.register_dialect('tsv_no_quote', delimiter='\t', quoting=csv.QUOTE_NONE, quotechar='')
+
 
 def unicode_csv_reader(filename, **kw):
     for line in csv.reader(filename, **kw):
--- scripts/geodata/i18n/languages.py
+++ scripts/geodata/i18n/languages.py
@@ -7,7 +7,7 @@ from collections import defaultdict, OrderedDict
 this_dir = os.path.realpath(os.path.dirname(__file__))
 sys.path.append(os.path.realpath(os.path.join(os.pardir, os.pardir)))
 
-from geodata.unicode_csv import unicode_csv_reader
+from geodata.csv_utils import unicode_csv_reader
 
 LANGUAGES_DIR = os.path.join(this_dir, os.pardir, os.pardir, os.pardir,
                              'resources', 'language')
