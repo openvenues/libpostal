bd31dc99f28a933ec4e4ad95480f1db23e3cbe25
Al <albarrentine@gmail.com>
2015-08-22 13:53:44 -0400

    [mv] csv_utils

--- scripts/geodata/csv_utils.py
+++ scripts/geodata/csv_utils.py
@@ -1,6 +1,14 @@
 import csv
+import re
+from encoding import safe_encode, safe_decode
+
+newline_regex = re.compile('\r\n|\r|\n')
 
 csv.register_dialect('tsv_no_quote', delimiter='\t', quoting=csv.QUOTE_NONE, quotechar='')
+
+
+def tsv_string(s):
+    return safe_encode(newline_regex.sub(u', ', safe_decode(s).strip()).replace(u'\t', u' '))
 
 
 def unicode_csv_reader(filename, **kw):
