85255299683ea28f2b215d74890b51a14f2553b2
Al <albarrentine@gmail.com>
2015-09-06 21:03:01 -0700

    [osm] Not requiring qualified name tags to process OSM toponyms

--- scripts/geodata/osm/osm_address_training_data.py
+++ scripts/geodata/osm/osm_address_training_data.py
@@ -625,7 +625,7 @@ def build_toponym_training_data(language_rtree, infile, out_dir):
     writer = csv.writer(f, 'tsv_no_quote')
 
     for key, value in parse_osm(infile):
-        if not sum((1 for k, v in value.iteritems() if k.startswith('name:'))) > 0:
+        if not sum((1 for k, v in value.iteritems() if k.startswith('name'))) > 0:
             continue
 
         try:
