644eeb74c6f709842e5609cbf1d95243472aeb25
Al <albarrentine@gmail.com>
2015-11-27 11:17:53 -0500

    [fix] import

--- scripts/geodata/osm/osm_address_training_data.py
+++ scripts/geodata/osm/osm_address_training_data.py
@@ -64,7 +64,7 @@ from geodata.i18n.languages import *
 from geodata.address_formatting.formatter import AddressFormatter
 from geodata.osm.extract import *
 from geodata.polygons.language_polys import *
-from geodata.polygons.reverse_geocode import OSMReverseGeocoder, NeighborhoodReverseGeocoder
+from geodata.polygons.reverse_geocode import *
 from geodata.i18n.unicode_paths import DATA_DIR
 
 from geodata.csv_utils import *
