43b212a09b8f28fdb7bd7e287c851686da025fef
Al <albarrentine@gmail.com>
2015-12-14 14:57:11 -0500

    [fix] size_t in benchmark script

--- src/bench.c
+++ src/bench.c
@@ -43,7 +43,7 @@ int main(int argc, char **argv) {
     options.num_languages = 1;
     options.languages = languages;
 
-    uint64_t num_expansions;
+    size_t num_expansions;
 
     char **strings;
     char *normalized;
