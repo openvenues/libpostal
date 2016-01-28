8702ee931c8991c8a09d29c3ae9f72c6e643918b
Al <albarrentine@gmail.com>
2016-01-28 16:37:29 -0500

    [tests] Main test program and Makefile

---
+++ test/test.c
@@ -0,0 +1,17 @@
+#include "greatest.h"
+
+SUITE_EXTERN(libpostal_expansion_tests);
+SUITE_EXTERN(libpostal_transliteration_tests);
+SUITE_EXTERN(libpostal_numex_tests);
+
+GREATEST_MAIN_DEFS();
+
+
+int main(int argc, char **argv) {
+    GREATEST_MAIN_BEGIN();
+
+    RUN_SUITE(libpostal_expansion_tests);
+    RUN_SUITE(libpostal_transliteration_tests);
+    RUN_SUITE(libpostal_numex_tests);
+    GREATEST_MAIN_END();
+}
