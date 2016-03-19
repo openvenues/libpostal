5e2d9f371e08d8d8ef5dfb70136c49d7229775c9
Al <albarrentine@gmail.com>
2016-03-18 20:36:22 -0400

    [numex] Moving numex script to a different subpackage, adding function for creating ordinals

--- scripts/geodata/numbers/numex.py
+++ scripts/geodata/numbers/numex.py
@@ -7,13 +7,14 @@ this_dir = os.path.realpath(os.path.dirname(__file__))
 sys.path.append(os.path.realpath(os.path.join(this_dir, os.pardir, os.pardir)))
 
 from geodata.encoding import safe_encode
-from unicode_paths import DATA_DIR
+from geodata.i18n.unicode_paths import DATA_DIR
 
 
 class InvalidNumexRuleException(Exception):
     pass
 
-NUMEX_DATA_DIR = os.path.join(DATA_DIR, 'numex')
+NUMEX_DATA_DIR = os.path.join(this_dir, os.pardir, os.pardir, os.pardir,
+                              'resources', 'numex')
 
 NUMEX_RULES_FILE = os.path.join(this_dir, os.pardir, os.pardir, os.pardir, 'src', 'numex_data.c')
 
