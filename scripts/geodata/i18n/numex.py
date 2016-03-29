2a2d1738a3f8a1ee2b75c10cbe967f0ff6e267b2
Al <albarrentine@gmail.com>
2016-03-29 11:15:24 -0400

    [fix] path for running numex.py

--- scripts/geodata/i18n/numex.py
+++ scripts/geodata/i18n/numex.py
@@ -4,7 +4,7 @@ import sys
 import ujson as json
 
 this_dir = os.path.realpath(os.path.dirname(__file__))
-sys.path.append(os.path.realpath(os.path.join(os.pardir, os.pardir)))
+sys.path.append(os.path.realpath(os.path.join(this_dir, os.pardir, os.pardir)))
 
 from geodata.encoding import safe_encode
 from unicode_paths import DATA_DIR
