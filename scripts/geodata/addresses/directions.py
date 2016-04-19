e13c536b0309a0dd88ab0f3a0ddcbf8c08545119
Al <albarrentine@gmail.com>
2016-04-19 17:05:10 -0400

    [addresses] different dictionaries for sampling cardinal/unit directions, not converting None to a string

--- scripts/geodata/addresses/directions.py
+++ scripts/geodata/addresses/directions.py
@@ -3,7 +3,9 @@ from geodata.addresses.numbering import NumericPhrase
 
 class RelativeDirection(NumericPhrase):
     key = 'directions'
+    dictionaries = ['unit_directions']
 
 
 class CardinalDirection(NumericPhrase):
     key = 'cardinal_directions'
+    dictionaries = ['cardinal_directions']
