b8e43fa7f847df66c3e68a0607497f77ff1200e8
Al <albarrentine@gmail.com>
2016-05-23 15:01:58 -0400

    [fix] args again

--- scripts/geodata/osm/osm_address_training_data.py
+++ scripts/geodata/osm/osm_address_training_data.py
@@ -499,7 +499,7 @@ if __name__ == '__main__':
         subdivisions_rtree = OSMSubdivisionReverseGeocoder.load(args.subdivisions_rtree_dir)
 
     buildings_rtree = None
-    if args.buildingss_rtree_dir:
+    if args.buildings_rtree_dir:
         buildings_rtree = OSMBuildingReverseGeocoder.load(args.buildings_rtree_dir)
 
     geonames = None
