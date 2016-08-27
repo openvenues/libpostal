6723fff9b40767a25db1ce3cd1f4108876eabc92
Al <albarrentine@gmail.com>
2016-08-27 00:23:47 -0400

    [fix] unit phrases

--- scripts/geodata/openaddresses/formatter.py
+++ scripts/geodata/openaddresses/formatter.py
@@ -42,8 +42,8 @@ class OpenAddressesFormatter(object):
 
     for (lang, dictionary_type), values in six.iteritems(address_phrase_dictionaries.phrases):
         if dictionary_type == 'unit_types_numbered':
-            unit_phrases = itertools.chain(*[safe_encode(p) for p in values if len(p) > 2])
-            pattern = re.compile(r'\b(?:{})\s+(?:#?\s*)(?:[\d]+|[a-z]|[a-z]\-?[\d]+|[\d]+\-?[a-z])\s*$'.format(six.u('|').join(unit_phrases)),
+            unit_phrases = [safe_encode(p) for p in itertools.chain(*values) if len(p) > 2]
+            pattern = re.compile(r'\b(?:{})\s+(?:#?\s*)(?:[\d]+|[a-z]|[a-z]\-?[\d]+|[\d]+\-?[a-z])\s*$'.format(safe_encode('|').join(unit_phrases)),
                                  re.I | re.UNICODE)
             unit_type_regexes[lang] = pattern
 
