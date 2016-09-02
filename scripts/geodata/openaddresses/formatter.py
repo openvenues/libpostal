a4a09fcb3e1a479cc64265698140f908126f21e4
Al <albarrentine@gmail.com>
2016-09-02 03:39:19 -0400

    [openaddresses] don't allow postcodes that are all zeroes with a dash (Poland, US ZIP+4)

--- scripts/geodata/openaddresses/formatter.py
+++ scripts/geodata/openaddresses/formatter.py
@@ -81,7 +81,7 @@ class OpenAddressesFormatter(object):
             '''
             Postcodes that are all zeros are improperly-formatted NULL values
             '''
-            return not all((c == '0' for c in postcode))
+            return not all((c in ('0', '-') for c in postcode))
 
         @classmethod
         def validate_street(cls, street):
