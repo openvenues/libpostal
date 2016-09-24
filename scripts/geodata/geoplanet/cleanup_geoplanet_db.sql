5a500820552095139236f0f8da44e2772e82ebf2
Al <albarrentine@gmail.com>
2016-09-24 02:16:00 -0400

    [geoplanet] removing the Ankara local admins

--- scripts/geodata/geoplanet/cleanup_geoplanet_db.sql
+++ scripts/geodata/geoplanet/cleanup_geoplanet_db.sql
@@ -446,25 +446,3 @@
     where parent_id = 2347289 -- Istanbul (province)
     and place_type = "County";
 
-    -- Ankara
-    update places
-    set parent_id = 2347263
-    where id = 2343732;
-
-    update admins
-    set county_id = 0
-    where id = 2343732;
-
-    -- metropolitan districts of Ankara
-    update places
-    set parent_id = 2343732, -- Ankara
-    place_type = "LocalAdmin"
-    where id in (
-        29390074, -- Altındağ
-        29390201, -- Çankaya
-        29390321, -- Etimesgut
-        29390485, -- Keçiören
-        29390552, -- Mamak
-        29390757, -- Sincan
-        29390874 -- Yenimahalle
-    );
