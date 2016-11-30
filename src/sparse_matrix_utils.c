f5a6bd0f364c42efaceafc0947bb804bb7a661b9
Al <albarrentine@gmail.com>
2016-11-30 10:15:12 -0800

    [fix] sparse_matrix_new_from_matrix uses new matrix types

--- src/sparse_matrix_utils.c
+++ src/sparse_matrix_utils.c
@@ -3,12 +3,12 @@
 #include "matrix.h"
 
 
-sparse_matrix_t *sparse_matrix_new_from_matrix(matrix_t *matrix) {
+sparse_matrix_t *sparse_matrix_new_from_matrix(double_matrix_t *matrix) {
     sparse_matrix_t *sparse = sparse_matrix_new_shape(matrix->m, matrix->n);
 
     for (size_t i = 0; i < matrix->m; i++) {
         for (size_t j = 0; j < matrix->n; j++) {    
-            double value = matrix_get(matrix, i, j);
+            double value = double_matrix_get(matrix, i, j);
             if (!double_equals(value, 0.0)) {
                 sparse_matrix_append(sparse, j, value);
             }
