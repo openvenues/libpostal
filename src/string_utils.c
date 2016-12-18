3ac2c93e1cfdc1639decd5f541cafae76423e86e
Al <albarrentine@gmail.com>
2016-12-18 15:26:58 -0500

    [utils] using renaming char_array_append_vjoined to char_array_add_vjoined to follow convention that add_* calls NUL-terminate while append_* calls do not

--- src/string_utils.c
+++ src/string_utils.c
@@ -597,7 +597,7 @@ inline void char_array_add_len(char_array *array, char *str, size_t len) {
 }
 
 
-void char_array_append_vjoined(char_array *array, char *separator, bool strip_separator, int count, va_list args) {
+void char_array_add_vjoined(char_array *array, char *separator, bool strip_separator, int count, va_list args) {
     if (count <= 0) {
         return;        
     }
@@ -627,7 +627,7 @@ void char_array_append_vjoined(char_array *array, char *separator, bool strip_se
 inline void char_array_add_joined(char_array *array, char *separator, bool strip_separator, int count, ...) {
     va_list args;
     va_start(args, count);
-    char_array_append_vjoined(array, separator, strip_separator, count, args);
+    char_array_add_vjoined(array, separator, strip_separator, count, args);
     va_end(args);
 }
 
@@ -635,7 +635,7 @@ inline void char_array_cat_joined(char_array *array, char *separator, bool strip
     char_array_strip_nul_byte(array);
     va_list args;
     va_start(args, count);
-    char_array_append_vjoined(array, separator, strip_separator, count, args);
+    char_array_add_vjoined(array, separator, strip_separator, count, args);
     va_end(args);
 }
 
