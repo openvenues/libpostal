96e1ca5e896c4a5913971f7a6e09115dc567c939
Al <albarrentine@gmail.com>
2017-04-02 13:48:46 -0400

    [utils] sparse_matrix_add_unique_columns_alias, adds the actual column indices to hashtable/array and aliases those in the table from 1 to N (where N is the number of unique columns in this batch). This way it's compatible with smaller matrices of batch weights.

--- src/sparse_matrix_utils.h
+++ src/sparse_matrix_utils.h
@@ -8,6 +8,8 @@
 
 sparse_matrix_t *sparse_matrix_new_from_matrix(double_matrix_t *matrix);
 uint32_array *sparse_matrix_unique_columns(sparse_matrix_t *matrix);
-bool sparse_matrix_add_unique_columns(sparse_matrix_t *matrix, khash_t(int_set) *unique_columns, uint32_array *array);
+bool sparse_matrix_add_unique_columns(sparse_matrix_t *matrix, khash_t(int_uint32) *unique_columns, uint32_array *array);
+bool sparse_matrix_alias_columns(sparse_matrix_t *matrix, khash_t(int_uint32) *unique_columns);
+bool sparse_matrix_add_unique_columns_alias(sparse_matrix_t *matrix, khash_t(int_uint32) *unique_columns, uint32_array *array);
 
 #endif
