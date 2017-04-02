ed05aaabb1c5d5b89e72e852934251c8768715f4
Al <albarrentine@gmail.com>
2017-04-02 13:51:45 -0400

    [utils] adding default chunk size to shuffle.h

--- src/shuffle.h
+++ src/shuffle.h
@@ -3,6 +3,11 @@
 
 #include <stdlib.h>
 #include <stdbool.h>
+#include <stdint.h>
+
+#define CHUNK_SIZE_MB UINT64_C(1024) * UINT64_C(1024)
+#define CHUNK_SIZE_GB UINT64_C(1024) * (CHUNK_SIZE_MB)
+#define DEFAULT_SHUFFLE_CHUNK_SIZE UINT64_C(2) * (CHUNK_SIZE_GB)
 
 bool shuffle_file(char *filename);
 bool shuffle_file_chunked(char *filename, size_t parts);
