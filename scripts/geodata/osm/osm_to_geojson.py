88564d0d65bf882e6cd06746d026730c094bf621
Al <albarrentine@gmail.com>
2017-08-03 00:52:07 -0600

    [osm] connert OSM to GeoJSON script (better implementation than ogr2ogr, one feature per line, use libpostal's lat/lon fu)

---
+++ scripts/geodata/osm/osm_to_geojson.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import ujson as json
+
+this_dir = os.path.realpath(os.path.dirname(__file__))
+sys.path.append(os.path.realpath(os.path.join(os.pardir, os.pardir)))
+
+from geodata.coordinates.conversion import latlon_to_decimal
+from geodata.osm.extract import parse_osm
+
+
+def convert_osm_to_geojson(input_file, output_file):
+    out = open(output_file, 'w')
+    for key, props, deps in parse_osm(infile):
+
+        lat, lon = latlon_to_decimal(props.pop('lat', None), props.pop('lon', None))
+        if not lat or not lon:
+            continue
+        geojson = {
+            'type': 'Feature',
+            'geometry': {
+                'coordinates': [lon, lat]
+            },
+            'properties': props
+        }
+
+        out.write(json.dumps(geojson) + u'\n')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        sys.exit('Usage: python osm_to_geojson.py input_file output_file')
+
+    input_file, output_file = sys.argv[1], sys.argv[2]
+    convert_osm_to_geojson(input_file, output_file)
