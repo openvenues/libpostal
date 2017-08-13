52bdb646d93eded6448c7f4114a20bea686aa55b
Al <albarrentine@gmail.com>
2017-08-13 04:09:41 -0600

    [fix] make script executable

--- scripts/geodata/i18n/download_cldr.py
+++ scripts/geodata/i18n/download_cldr.py
@@ -4,11 +4,11 @@ import subprocess
 import sys
 import tempfile
 
-from unicode_paths import CLDR_DIR
-from geodata.file_utils import ensure_dir
-
 this_dir = os.path.realpath(os.path.dirname(__file__))
 sys.path.append(os.path.realpath(os.path.join(os.pardir, os.pardir)))
+
+from geodata.i18n.unicode_paths import CLDR_DIR
+from geodata.file_utils import ensure_dir
 
 CLDR_URL = 'http://www.unicode.org/Public/cldr/latest/core.zip'
 
