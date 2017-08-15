1dff7258e4071c786f26c4aaecd49d00a63b2ca8
Al <albarrentine@gmail.com>
2017-08-14 18:34:35 -0600

    [fix] adding back tokenize_raw convenience method as it's used in several places

--- scripts/geodata/text/tokenize.py
+++ scripts/geodata/text/tokenize.py
@@ -3,9 +3,12 @@ from geodata.text import _tokenize
 from geodata.text.token_types import token_types
 
 
-
 def tokenize(s, whitespace=False):
     u = safe_decode(s)
     s = safe_encode(s)
     return [(safe_decode(s[start:start + length]), token_types.from_id(token_type))
             for start, length, token_type in _tokenize.tokenize(u, whitespace)]
+
+
+def tokenize_raw(s):
+    return tokenize(s, whitespace=True)
