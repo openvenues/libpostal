590f35aafe6a812cf0f08b5549f87163a4e910f9
Al <albarrentine@gmail.com>
2017-08-22 04:25:02 -0400

    [fix] arg again

--- scripts/geodata/spark/polygon_index.py
+++ scripts/geodata/spark/polygon_index.py
@@ -133,7 +133,7 @@ class PolygonIndexSpark(object):
                                               .reduceByKey(lambda x, y: x + y) \
                                               .mapValues(lambda polys: sorted(polys, key=cls.sort_key_tuple, reverse=cls.sort_reverse))
 
-        if buffer_levels:
+        if with_buffer_levels:
             return points_with_polys.mapValues(lambda polys: [p for p, level in sorted(polys, key=cls.sort_level)])
         else:
             return points_with_polys.mapValues(lambda polys: sorted(polys, key=cls.sort_level))
